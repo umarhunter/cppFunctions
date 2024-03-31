2a5dea6e9510bc35146a4cd971f784d68a2b045f
umarhunter <umar.faruque54@myhunter.cuny.edu>
2024-03-30 23:51:56 -0400

    fix twos_threes_fives.cpp

--- twos_threes_fives.cpp
+++ twos_threes_fives.cpp
@@ -1,4 +1,3 @@
-//
 // Assuming 2, 3 & 5 are the only prime numbers, generate a sequence of numbers where those are the only factors.
 //
 #include <cstdio>
@@ -13,37 +12,29 @@ bool isDuplicate(unsigned int num, const unsigned int* array, size_t size){
     return false;
 }
 
-unsigned int getNextPrime(unsigned int curr_prime, unsigned int* array, size_t size){
-    unsigned int new_num, x, y, z = 0;
-//
-//    x = 2 * curr_prime;
-//    y = 3 * curr_prime;
-//    z = 5 * curr_prime;
-//
-    int min = 0;
+unsigned int getNextPrime(unsigned int* array, size_t size){
     for(auto x = 0; x < size; ++x){ // iterate through {1,2,3,4,5,6,8,9...}
-        if(array[x]*2 < curr_prime && !(isDuplicate()))
+        unsigned int next_prime;
+        unsigned int x2 = array[x]*2;
+        unsigned int x3 = array[x]*3;
+        unsigned int x5 = array[x]*5;
+        if(x2 > curr_prime && !(isDuplicate(x2, array, size))){
+            if(x2 < x3 && x2 < x5)
+                curr_prime = x2;
+        }
+        else if(x3 > curr_prime && !(isDuplicate(x3, array, size))){
+            if(x3 < x5)
+                curr_prime = x3;
+        }
+        else if(x5 < curr_prime && !(isDuplicate(x5, array, size))){
+            if(x2 < x3 && x2 < x5)
+                curr_prime = x2;
+        }
+        else{
 
+        }
     }
-//    int x2 = 2 * prev_prime;
-//    int y2 = 3 * prev_prime;
-//    int z2 = 5 * prev_prime;
-//    if(isDuplicate(x2, array, size)){
-//
-//    }
 
-
-    if (x == y && y == z) {
-        new_num = x;
-    } else if (x == y) {
-        new_num = x < z ? x : z;
-    } else if (x == z) {
-        new_num = x < y ? x : y;
-    } else if (y == z) {
-        new_num = y < x ? y : x;
-    } else {
-        new_num = x < y ? (x < z ? x : z) : (y < z ? y : z);
-    }
     curr_prime = new_num;
     printf("new_num: %d\n", new_num);
     return new_num;
@@ -57,7 +48,7 @@ void addPrime(const unsigned int curr_num, unsigned int* array, size_t size){
     if(curr_num == 3){ array[3] = 4; curr_prime = 4; prev_prime = curr_prime--; return; }
     if(curr_num == 4){ array[4] = 5; curr_prime = 5; prev_prime = curr_prime--; return; }
     printf("curr_num: %d\n", curr_num);
-    array[curr_num] = getNextPrime(curr_prime, array, size);
+    array[curr_num] = getNextPrime(array, size);
 }
 
 void printSequence(unsigned int *array, size_t size) {
@@ -67,8 +58,6 @@ void printSequence(unsigned int *array, size_t size) {
 }
 
 int main(){
-    //const unsigned int primes[3] = {2,3,5}; // list of what we consider primes
-
     unsigned int size;
     std::scanf(" %u", &size);
     //std::printf("%u", size);
